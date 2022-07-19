01ae578881a741ca6a356b02434a6347fd0f3f37
PhongLuong99 <77917776+PhongLuong99@users.noreply.github.com>
2022-07-19 15:02:41 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const server = express();
+
+server.get('/echo', (req, res, next) => {
+    res.status(200).json({
+        id: 1,
+        description: 'hello',
+        name: 'phong'
+    })
+  })
+
+
+const PORT = process.env.PORT || 5001;
+
+server.listen(PORT, () => {
+  console.log('JSON Server is running');
+});
